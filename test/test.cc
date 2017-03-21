2ce3d8bd1b626619688d3b9671345cdf6dfe337a
Nicolas Pierre <nicolas.pierre@cern.ch>
2017-03-21 17:17:34 +0100

    Added ‘Progress’ statement

--- test/test.cc
+++ test/test.cc
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 #include "TDjangoh.h"
 
+# define VERBOSE 0
+# define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+# define PBWIDTH 60
+# define NEVENTS 50000
+
 using namespace std;
+
+void printProgress (int event, int total)
+{
+    string points[4] = {"   ",".  ",".. ","..."};
+    double percentage = double(event)/double(total);
+    int val = (int) (percentage * 100);
+    int lpad = (int) (percentage * PBWIDTH);
+    int rpad = PBWIDTH - lpad;
+    printf ("\r Progress%s %3d%% [%.*s%*s] (%d/%d)", points[int(event/16)%4].c_str(), val, lpad, PBSTR, rpad, "", event, total);
+    fflush (stdout);
+}
 
 int main()
 {
@@ -21,41 +38,47 @@ int main()
   tDjangoh->Initialize("mu-", 1.0, 1.0, 160.0, 0.0);
   cout << "Initialized !" << endl;
 
-  for(int i=0; i<1000; i++)
+  cout << "\n\nEvents Generation (" << NEVENTS << " events).." << endl;
+  for(int i=0; i<NEVENTS; i++)
   {
 
     tDjangoh->Configure("mu-", 1.0, 1.0, 140+rand()%40, 0.0);
 
-    cout << "\n\nEvent Generation (N°" << i << ").." << endl;
+    printProgress (i+1,NEVENTS);
     tDjangoh->GenerateEvent();
-    cout << "Event Generated !\n" << endl;
 
-    nb = tDjangoh->GetN();
-    cout << "Total number of particles : " << nb << endl;
-    for(int j=0; j<nb; j++)
+    if(VERBOSE)
     {
-    	cout
-    	<< tDjangoh->GetK(j,1) << " "
-    	<< tDjangoh->GetK(j,2) << " "
-    	<< tDjangoh->GetK(j,3) << " "
-    	<< tDjangoh->GetK(j,4) << " "
-    	<< tDjangoh->GetK(j,5) << " \n"
-    	<< tDjangoh->GetP(j,1) << " "
-      << tDjangoh->GetP(j,2) << " "
-      << tDjangoh->GetP(j,3) << " "
-      << tDjangoh->GetP(j,4) << " "
-      << tDjangoh->GetP(j,5) << " \n"
-      << tDjangoh->GetV(j,1) << " "
-      << tDjangoh->GetV(j,2) << " "
-      << tDjangoh->GetV(j,3) << " "
-      << tDjangoh->GetV(j,4) << " "
-      << tDjangoh->GetV(j,5) << " \n" << endl;
-    }
-    x = tDjangoh->GetX();
-    y = tDjangoh->GetY();
-    Q2 = tDjangoh->GetQ2();
-    cout << "Xbj : " << x << " y : " << y << " Q2 : " << Q2 << "\n\n" << endl;
+       nb = tDjangoh->GetN();
+       cout << "Total number of particles : " << nb << endl;
+       for(int j=0; j<nb; j++)
+       {
+        	cout
+    		<< tDjangoh->GetK(j,1) << " "
+    		<< tDjangoh->GetK(j,2) << " "
+    		<< tDjangoh->GetK(j,3) << " "
+    		<< tDjangoh->GetK(j,4) << " "
+    		<< tDjangoh->GetK(j,5) << " \n"
+    		<< tDjangoh->GetP(j,1) << " "
+      		<< tDjangoh->GetP(j,2) << " "
+      		<< tDjangoh->GetP(j,3) << " "
+    		<< tDjangoh->GetP(j,4) << " "
+      		<< tDjangoh->GetP(j,5) << " \n"
+      		<< tDjangoh->GetV(j,1) << " "
+      		<< tDjangoh->GetV(j,2) << " "
+      		<< tDjangoh->GetV(j,3) << " "
+      		<< tDjangoh->GetV(j,4) << " "
+      		<< tDjangoh->GetV(j,5) << " \n" << endl;
+    	}
+    	x = tDjangoh->GetX();
+    	y = tDjangoh->GetY();
+    	Q2 = tDjangoh->GetQ2();
+    	cout << "Xbj : " << x << " y : " << y << " Q2 : " << Q2 << "\n\n" << endl;
+     }
+
   }
+
+  cout << "Events Generated !" << endl;
 
   tDjangoh->EndRecap();
 
