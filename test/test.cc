3ffe4fb148b0a9740477cc4417ac8c0cadb3dc3b
Nicolas Pierre <nicolas.pierre@cern.ch>
2017-06-02 14:12:42 +0200

    Added colours to cout

--- test/test.cc
+++ test/test.cc
@@ -4,6 +4,28 @@
 
 #include "TDjangoh.h"
 
+// COLORS
+
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLD(x) "\x1B[1m" x RST
+#define UNDL(x) "\x1B[4m" x RST
+
 # define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 # define PBWIDTH 60
 
@@ -16,7 +38,7 @@ void printProgress (int event, int total)
     int val = (int) (percentage * 100);
     int lpad = (int) (percentage * PBWIDTH);
     int rpad = PBWIDTH - lpad;
-    printf ("\r Progress%s %3d%% [%.*s%*s]", points[int(event/16)%6].c_str(), val, lpad, PBSTR, rpad, "");
+    printf (FGRN("\r Progress%s %3d%% [%.*s%*s]"), points[int(event/16)%6].c_str(), val, lpad, PBSTR, rpad, "");
     fflush (stdout);
 }
 
@@ -25,9 +47,9 @@ int main(int argc,char *argv[])
 
   if(argc < 3)
   {
-    cout << "ERROR : Wrong number of arguments" << endl;
-    cout << "At least expected 3, received " << argc << " !" << endl;
-    cout << "USAGE : \n ./rc_calc input_file nb_evts + flags" << endl;
+    cout << BOLD(FRED("ERROR : Wrong number of arguments")) << endl;
+    cout << BOLD(FRED("At least expected 3, received " << argc << " !")) << endl;
+    cout << BOLD(FRED("USAGE : \n ./rc_calc input_file nb_evts + flags")) << endl;
     return 1;
   }
 
@@ -67,15 +89,16 @@ int main(int argc,char *argv[])
   float Q2;
   int NEVENTS = atoi(argv[2]);
 
-  cout << "Instance creation.." << endl;
+  cout << FCYN("Instance creation..") << endl;
   tDjangoh = new TDjangoh();
-  cout << "Instance created !" << endl;
+  cout << FCYN("Instance created !") << endl;
 
-  cout << "Initialization.." << endl;
-  tDjangoh->Initialize(argv[1]);
-  cout << "Initialized !" << endl;
+  cout << FCYN("Initialization..") << endl;
+  tDjangoh->ReadXMLFile(argv[1]);
+  tDjangoh->Initialize();
+  cout << FCYN("Initialized !") << endl;
 
-  cout << "\n\nEvents Generation (" << NEVENTS << " events).." << endl;
+  cout << FCYN("\n\nEvents Generation (" << NEVENTS << " events)..") << endl;
   for(int i=0; i<NEVENTS; i++)
   {
 
@@ -95,7 +118,7 @@ int main(int argc,char *argv[])
     if(VERBOSE>2)
     {
        nb = tDjangoh->GetN();
-       cout << "Total number of particles : " << nb << endl;
+       cout << FCYN("Total number of particles : ") << nb << endl;
        for(int j=0; j<nb; j++)
        {
         	cout
@@ -123,7 +146,7 @@ int main(int argc,char *argv[])
 
   }
 
-  cout << "\n\nEvents Generated !" << endl;
+  cout << FCYN("\n\nEvents Generated !") << endl;
 
   tDjangoh->EndRecap();
 
