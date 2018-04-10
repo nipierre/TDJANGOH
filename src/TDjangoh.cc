ae51d65033b7fff2481c6877de979c9fab789cf9
npierre <nicolas.pierre@cern.ch>
2018-04-10 14:49:39 +0200

    Test.

--- src/TDjangoh.cc
+++ src/TDjangoh.cc
@@ -1513,7 +1513,7 @@ void TDjangoh::WriteFSInFile()
 {
   ofstream finalState("finalState.txt", ofstream::out | ofstream::app);
 
-  if(fLujets->K[0][-1])
+  if(fLujets->K[0][-1]==1)
   {
     Int_t numpart = fLujets->N;
 
