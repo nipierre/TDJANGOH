1f4f4029c28b62e90beef5f1b7d4fc6fb6e71c94
npierre <nicolas.pierre@cern.ch>
2018-04-16 12:24:42 +0200

    TesT.

--- src/sophia.f
+++ src/sophia.f
@@ -186,13 +186,14 @@ C...Hadronic final state from SOPHIA
       DO 20 IP=1,NP
         IF (ABS(LLIST(IP)).LT.50) THEN
           IP1=IP1+1
-          WRITE(6,*) P_S(IP,1), P_S(IP,2), P_S(IP,3), P_S(IP,4)
+          WRITE(33,*) IP, IP1
+          WRITE(33,*) P_S(IP,1), P_S(IP,2), P_S(IP,3), P_S(IP,4)
           P(IP1,1)=P_S(IP,1)
           P(IP1,2)=P_S(IP,2)
           P(IP1,3)=P_S(IP,3)
           P(IP1,4)=P_S(IP,4)
           P(IP1,5)=P_S(IP,5)
-          WRITE(6,*) P(IP,1), P(IP,2), P(IP,3), P(IP,4)
+          WRITE(33,*) P(IP1,1), P(IP1,2), P(IP1,3), P(IP1,4)
           K(IP1,1)=1
           K(IP1,2)=ICON_SIB_PDG(LLIST(IP))
           K(IP1,3)=0
