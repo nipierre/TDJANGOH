db1a1f9cb39a2720a85dd8b95afd9dca34e916d7
npierre <nicolas.pierre@cern.ch>
2018-04-16 15:12:58 +0200

    TesT.

--- src/sophia.f
+++ src/sophia.f
@@ -159,8 +159,8 @@ C...scattered lepton
       DO 12 J=1,5
         P(IP1,J)=PHEP(J,1)
  12   CONTINUE
-      WRITE(33,*) IP1
-      WRITE(33,*) P(IP1,1),P(IP1,2),P(IP1,3),P(IP1,4),P(IP1,5)
+C      WRITE(33,*) IP1
+C      WRITE(33,*) P(IP1,1),P(IP1,2),P(IP1,3),P(IP1,4),P(IP1,5)
       K(IP1,1)=ISTHEP(1)
       K(IP1,2)=IDHEP(1)
       K(IP1,3)=1
@@ -173,7 +173,7 @@ C...radiative photon
           P(IP1,J)=PHEP(J,3)
  13     CONTINUE
         WRITE(33,*) IP1
-        WRITE(33,*) P(IP1,1),P(IP1,2),P(IP1,3),P(IP1,4),P(IP1,5)
+C        WRITE(33,*) P(IP1,1),P(IP1,2),P(IP1,3),P(IP1,4),P(IP1,5)
         K(IP1,1)=ISTHEP(3)
         K(IP1,2)=IDHEP(3)
         K(IP1,3)=1
@@ -190,14 +190,14 @@ C...Hadronic final state from SOPHIA
       DO 20 IP=1,NP
         IF (ABS(LLIST(IP)).LT.50) THEN
           IP1=IP1+1
-          WRITE(33,*) IP, IP1
-          WRITE(33,*) P_S(IP,1),P_S(IP,2),P_S(IP,3),P_S(IP,4),P_S(IP,5)
+C          WRITE(33,*) IP, IP1
+C          WRITE(33,*) P_S(IP,1),P_S(IP,2),P_S(IP,3),P_S(IP,4),P_S(IP,5)
           P(IP1,1)=P_S(IP,1)
           P(IP1,2)=P_S(IP,2)
           P(IP1,3)=P_S(IP,3)
           P(IP1,4)=P_S(IP,4)
           P(IP1,5)=P_S(IP,5)
-          WRITE(33,*) P(IP1,1),P(IP1,2),P(IP1,3),P(IP1,4),P_S(IP,5)
+C          WRITE(33,*) P(IP1,1),P(IP1,2),P(IP1,3),P(IP1,4),P_S(IP,5)
           K(IP1,1)=1
           K(IP1,2)=ICON_SIB_PDG(LLIST(IP))
           K(IP1,3)=0
@@ -521,6 +521,8 @@ c... consider only stable particles:
          if (abs(LLIST(i)).lt.10000) then
           istable = istable+1
           LLIST(istable) = LLIST(i)
+          WRITE(33,*) i
+          WRITE(33,*) SP(i,1),SP(i,2),SP(i,3),SP(i,4),SP(i,5)
           SP(istable,1) = SP(i,1)
           SP(istable,2) = SP(i,2)
           SP(istable,3) = SP(i,3)
