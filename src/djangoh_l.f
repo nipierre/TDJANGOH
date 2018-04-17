fe23477828881fb520b6857c1de0521dad3ab192
npierre <nicolas.pierre@cern.ch>
2018-04-17 11:15:16 +0200

    Revert "TesT."
    This reverts commit e13f3f44dec1e37aea5e464200927b8fca08f374.

--- src/djangoh_l.f
+++ src/djangoh_l.f
@@ -179,8 +179,7 @@ c
 c%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 c
       SUBROUTINE DJGINIT(LEPIN,PLZ,PPZ,INTER)
-      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      IMPLICIT INTEGER (I-N)
+
 chs..from LINIT, L61
 chs..updated for L62, L63, L65
 C...Initialize for an incoming lepton (type LEPIN, momentum pz=PLZ)
@@ -2249,8 +2248,8 @@ ckc..virtual boson (non-radiative case)
 C%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
       SUBROUTINE DJGLEV
-      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      IMPLICIT INTEGER (I-N)
+C      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+C      IMPLICIT INTEGER (I-N)
 Chs...Restore event record from HERACLES when hadronization has failed
       COMMON /LINTRL/ PSAVE(3,4,5),KSAVE(4),XMIN,XMAX,YMIN,YMAX,
      &Q2MIN,Q2MAX,W2MIN,W2MAX,ILEP,INU,IG,IZ
